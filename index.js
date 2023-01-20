ee732c861059a787fbf4dad3d785cd09e2d8cfdf
Cheetah83 <ssenyange86@yahoo.com>
2023-01-20 17:53:45 +0200

    created layout and a live server for the site

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const { about } = require('./contollers/about');
+const { create } = require('./contollers/create');
+const { details } = require('./contollers/details');
+const { home } = require('./contollers/home');
+const { notFound } = require('./contollers/notFound');
+
+const app = express();
+
+app.engine('hbs', hbs.create({
+  extname: '.hbs'
+}).engine);
+app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+
+app.get('/', home);
+app.get('/about', about);
+app.get('/create', create);
+app.get('/details/:id', details);
+
+app.all('*', notFound);
+
+app.listen(3000, () => console.log('Server is listening on port 3000...'));
